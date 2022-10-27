6235c3aa4f9999745b683434619483870045abf3
zihanxu3 <47161813+zihanxu3@users.noreply.github.com>
2022-10-27 10:37:20 -0400

    refactoring codebase

---
+++ frontend/pydnd/src/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { Application } from './App';
+import { BodyWidget } from './components/BodyWidget';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+var app = new Application();
+root.render(
+  <React.StrictMode>
+    <BodyWidget app={app} />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
